3e67f42084b34503b896c3b7d7957743d29f2518
Marzdevs <alzokarimarwa@gmail.com>
2023-09-20 15:46:38 -0500

    added more to it

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-from googletrans import Translator
-translator = Translator()
-# <Translated src=ko dest=en text=Good evening. pronunciation=Good evening.>
-out = translator.translate('hi', dest='ar')
+from googletrans import Translator, LANGUAGES
 
-print(out.text)
+
+def translate_text():
+    translator = Translator()
+
+    # Display supported languages
+    print("Supported languages:")
+    for lang_code, lang_name in LANGUAGES.items():
+        print(f"{lang_code}: {lang_name}")
+
+    source_lang = input("Enter the source language code (e.g., 'en' for English): ").strip().lower()
+    target_lang = input("Enter the target language code (e.g., 'fr' for French): ").strip().lower()
+
+    while True:
+        text = input("Enter text to translate (or type 'exit' to quit): ").strip()
+        if text.lower() == 'exit':
+            break
+
+        try:
+            translated = translator.translate(text, src=source_lang, dest=target_lang)
+            print(f"Translation ({LANGUAGES[source_lang]} to {LANGUAGES[target_lang]}): {translated.text}")
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+
+
+if __name__ == "__main__":
+    translate_text()
